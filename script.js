c432e2c6cf0dd2e7484079abd8aa24f57ed52388
Michael Heard <compmike@gmail.com>
2015-02-26 19:37:38 -0500

    updated spacing issues

--- script.js
+++ script.js
@@ -7,29 +7,43 @@ $options.click(function() {
 });
 var output;
 var ytid;
+var input;
+var vmid;
+
+//Youtube Regex
+//http://www.rubular.com/r/j5oqOj62df
+var yt = /^.*(youtu.be\/|v\/|embed\/|watch\?|youtube.com\/user\/[^#]*#([^\/]*?\/)*)\??v?=?([^#\&\?]*).*([\w-]{11})/;
+
+//Vimeo Regex will match urls of the type below
+//http://www.rubular.com/r/cWKFAxHM30
+//Needs to support these formats:
+//https://vimeo.com/*
+//https://vimeo.com/channels/*/*
+//https://vimeo.com/groups/*/videos/*
+var vm = /vimeo.com\/(\d+)($|\/)/;
 
 //grab the link from the input field and stop default action
-var input;
-	var findService = function(service,url) {
-		if (service === "youtube") {
-			output = "*|YOUTUBE:[$vid="+ ytid + "]|*";
-			$("#output").html(output);
-		} else {
-			output = "unsupported service";
-			$("#output").append(output);
-			console.log("not youtube");
-		}
+var findService = function(service,url) {
+	if (service === "youtube") {
+		output = "*|YOUTUBE:[$vid="+ ytid + "]|*";
+		$("#output").html(output);
+	} else if(service === "vimeo"){
+		output = "*|VIMEO:[$vid="+ vmid + "]|*";
+		$("#output").html(output);
 	}
+}
 $('#submit').on("click", function(){
 	input = $('#input').val();
-	//http://www.rubular.com/r/y0oZBNSzNW
-	var yt = /^.*(youtu.be\/|v\/|embed\/|watch\?|youtube.com\/user\/[^#]*#([^\/]*?\/)*)\??v?=?([^#\&\?]*).*([\w-]{11})/;
-	ytid = input.match(yt)[4];
-	console.log(ytid);
 	if (input.match(yt) != null) {
+		ytid = input.match(yt)[4];
 		console.log("this is youtube");
 		findService("youtube",input);
-	} else {
+	}
+	if (input.match(vm) != null) {
+		vmid = input.match(vm)[1];
+		console.log("something");
+		console.log("this is not youtube");
+		findService("vimeo",input);
 	}
 
 	//find service
