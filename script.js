0ac5710d6354bde4b59a386c79a68a28d99d637e
Michael Heard <compmike@gmail.com>
2015-03-01 21:00:46 -0500

    options are now working, also made copy area into text area so copying wouldn't copy the style

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ var $ob = $('#optionsBlock');
 $options.click(function() {
 	$ob.toggleClass('show');
 });
-var output;
+var output = "...";
 var ytid;
 var input;
 var vmid;
 var vzid;
-var options = "";
 
 //Youtube Regex
 //http://www.rubular.com/r/j5oqOj62df
@@ -31,14 +30,14 @@ var vz = /vzaar.com\/(\d+)/;
 //grab the link from the input field and stop default action
 var findService = function(service,url) {
 	if (service === "youtube") {
-		output = "*|YOUTUBE:[$vid="+ ytid + options + "]|*";
-		$("#output").html(output);
+		output = "*|YOUTUBE:[$vid="+ ytid;
+		$("#output").html(output  + "]|*");
 	} else if(service === "vimeo"){
-		output = "*|VIMEO:[$vid="+ vmid + "]|*";
-		$("#output").html(output);
+		output = "*|VIMEO:[$vid="+ vmid;
+		$("#output").html(output + "]|*");
 	} else if(service === "vzaar"){
-		output = "*|VZAAR:[$vid="+ vzid + "]|*";
-		$("#output").html(output);
+		output = "*|VZAAR:[$vid=";
+		$("#output").html(output+ vzid + "]|*");
 	}
 }
 $('#submit').on("click", function(){
@@ -51,13 +50,13 @@ $('#submit').on("click", function(){
 	if (input.match(vm) != null) {
 		vmid = input.match(vm)[1];
 		console.log("something");
-		console.log("this is not youtube");
+		console.log("this is vimeo");
 		findService("vimeo",input);
 	}
 	if (input.match(vz) != null) {
 	vzid = input.match(vz)[1];
 	console.log("something");
-	console.log("this is not youtube");
+	console.log("this is vzaar");
 	findService("vzaar",input);
 	}
 	//find service
@@ -65,11 +64,36 @@ $('#submit').on("click", function(){
 
 //Options
 var addOptions = function() {
-		if($(this).val() === "Title") {
-		 options = options + ", $title=N";
+		var options = "";
+		var endMergeTag = "";
+		if(document.getElementById("width").checked) {
+			// var width = prompt("Enter a width, 450 is the max.");
+		 	options += ", $max_width=450"
 		}
-	console.log(options);
+		if(document.getElementById("title").checked) {
+		 options += ", $title=Y";
+		}
+		if(document.getElementById("border").checked) {
+		 options += ", $border=Y";
+		}
+		if(document.getElementById("trim_border").checked) {
+		 options += ", $trim_border=Y";
+		}
+		if(document.getElementById("ratings").checked) {
+		 options += ", $ratings=Y";
+		}
+		if(document.getElementById("views").checked) {
+		 options += ", $views=Y";
+		}
+		if(document.getElementById("watch_url").checked) {
+		 options += ", $watch_URL=Y";
+		}
+		var endMergeTag = "]|*";
+	  var completeMergeTag = output + options  + endMergeTag;
+	  document.getElementById('output').innerHTML = completeMergeTag;
+	console.log("options = " + options);
 }
+$("#form").on('change', addOptions);
+//run once at startup
 
-$("input[type=checkbox]").on("click",addOptions);
 })
